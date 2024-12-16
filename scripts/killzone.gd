123c442b85e74ddae9952ab08dab6b46ea28559a
braiaguilar <brianisaiasaguilar@gmail.com>
2024-12-16 00:08:52 -0300

    Remove loop on animations/Better comments/Better consts

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
+const DEATH_DELAY_TIME = 0.5
+
 func _on_body_entered(body: Node2D) -> void:
-	Engine.time_scale = 0.5
+	Engine.time_scale = DEATH_DELAY_TIME
 	body.get_node('AnimatedSprite2D').play("death")
 	body.get_node('CollisionShape2D').queue_free()
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+# Const and var
 const SPEED = 100.0
 const JUMP_VELOCITY = -350.0
+const SPRINT_MULTIPLIER = 1.6
 var jump_counter = 0
 var current_speed = SPEED
 var is_rolling = false
@@ -9,57 +11,54 @@ var is_rolling = false
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	# Conectamos la señal animation_finished al script.
-	animated_sprite.connect("animation_finished", self, "_on_animation_finished")
+    animated_sprite.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += (get_gravity() * 1.2) * delta
+    # Add gravity.
+    if not is_on_floor():
+        velocity += (get_gravity() * 1.2) * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jump_counter < 2:
-		velocity.y = JUMP_VELOCITY
-		jump_counter += 1
+    # Handle jump.
+    if Input.is_action_just_pressed("jump") and jump_counter < 2:
+        velocity.y = JUMP_VELOCITY
+        jump_counter += 1
 
-	if is_on_floor() and jump_counter >= 2:
-		jump_counter = 0
+    if is_on_floor() and jump_counter >= 2:
+        jump_counter = 0
 
-	# Sprint.
-	if Input.is_action_just_pressed("sprint") and is_on_floor():
-		current_speed = SPEED * 1.6
-	if Input.is_action_just_released("sprint"):
-		current_speed = SPEED
+    # Handle sprint.
+    if Input.is_action_just_pressed("sprint") and is_on_floor():
+        current_speed = SPEED * SPRINT_MULTIPLIER
+    if Input.is_action_just_released("sprint"):
+        current_speed = SPEED
 
-	# Handle movement.
-	var direction := Input.get_axis("move_left", "move_right")
+    # Handle movement.
+    var direction := Input.get_axis("move_left", "move_right")
 
-	if direction == -1:
-		animated_sprite.flip_h = true
-	if direction == 1:
-		animated_sprite.flip_h = false
+    if direction:
+        if direction == -1:
+            animated_sprite.flip_h = true
+        if direction == 1:
+            animated_sprite.flip_h = false
+        velocity.x = direction * current_speed
+    else:
+        velocity.x = move_toward(velocity.x, 0, current_speed)
 
-	if direction:
-		velocity.x = direction * current_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
+    # Animations.
+    if not is_rolling:
+        if is_on_floor():
+            if Input.is_action_just_pressed("roll"):
+                is_rolling = true
+                animated_sprite.play("roll")
+            elif direction:
+                animated_sprite.play("run")
+            else:
+                animated_sprite.play("idle")
+        else:
+            animated_sprite.play("air")
 
-	# Animations.
-	if not is_rolling:
-		if is_on_floor():
-			if Input.is_action_just_pressed("roll"):
-				is_rolling = true
-				animated_sprite.play("roll")
-			elif direction:
-				animated_sprite.play("run")
-			else:
-				animated_sprite.play("idle")
-		else:
-			animated_sprite.play("air")
+    move_and_slide()
 
-	move_and_slide()
-
-func _on_animation_finished() -> void:
-	# Detectar cuando termine la animación "roll".
-	if animated_sprite.animation == "roll":
-		is_rolling = false
+func _on_animation_finished(animation_name: String) -> void:
+    if animation_name == "roll":
+        is_rolling = false
