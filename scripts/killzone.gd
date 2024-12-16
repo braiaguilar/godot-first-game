a995c00aba97c085b9bcb8f2d7b86c17a686b4c9
braiaguilar <brianisaiasaguilar@gmail.com>
2024-12-16 01:09:57 -0300

    Better logic. Still with animation bugs

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-
 const DEATH_DELAY_TIME = 0.5
 
 func _on_body_entered(body: Node2D) -> void:
-	Engine.time_scale = DEATH_DELAY_TIME
-	body.get_node('AnimatedSprite2D').play("death")
-	body.get_node('CollisionShape2D').queue_free()
-	timer.start()
+    if body.has_method("die"):
+        Engine.time_scale = DEATH_DELAY_TIME
+        body.get_node("CollisionShape2D").queue_free()
+        body.die()
+        timer.start()
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
+    Engine.time_scale = 1.0
+    get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
-# Const and var
+# Constants
+const GRAVITY_INTENSITY = 1.2
 const SPEED = 100.0
 const JUMP_VELOCITY = -350.0
 const SPRINT_MULTIPLIER = 1.6
+const STATE_IDLE = "idle"
+const STATE_SPRINT = "sprint"
+const STATE_ROLL = "roll"
+const STATE_JUMP = "jump"
+const STATE_DEATH = "death"
+
+# Variables
 var jump_counter = 0
 var current_speed = SPEED
-var is_rolling = false
+var player_state = STATE_IDLE
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -14,51 +22,75 @@ func _ready() -> void:
     animated_sprite.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
-    # Add gravity.
-    if not is_on_floor():
-        velocity += (get_gravity() * 1.2) * delta
+    if player_state == STATE_DEATH:
+        return
 
-    # Handle jump.
-    if Input.is_action_just_pressed("jump") and jump_counter < 2:
+    apply_gravity(delta)
+    handle_jump()
+    handle_sprint()
+    handle_movement()
+    update_animation()
+    move_and_slide()
+
+func apply_gravity(delta: float) -> void:
+    if not is_on_floor():
+        velocity += (get_gravity() * GRAVITY_INTENSITY) * delta
+
+func handle_jump() -> void:
+    if Input.is_action_just_pressed(STATE_JUMP) and jump_counter < 2:
         velocity.y = JUMP_VELOCITY
         jump_counter += 1
 
     if is_on_floor() and jump_counter >= 2:
         jump_counter = 0
 
-    # Handle sprint.
-    if Input.is_action_just_pressed("sprint") and is_on_floor():
+func handle_sprint() -> void:
+    if Input.is_action_just_pressed(STATE_SPRINT) and is_on_floor():
         current_speed = SPEED * SPRINT_MULTIPLIER
-    if Input.is_action_just_released("sprint"):
+    if Input.is_action_just_released(STATE_SPRINT):
         current_speed = SPEED
 
-    # Handle movement.
-    var direction := Input.get_axis("move_left", "move_right")
-
-    if direction:
-        if direction == -1:
-            animated_sprite.flip_h = true
-        if direction == 1:
-            animated_sprite.flip_h = false
+func handle_movement() -> void:
+    var direction = Input.get_axis("move_left", "move_right")
+    if direction != 0:
+        animated_sprite.flip_h = direction < 0
         velocity.x = direction * current_speed
     else:
         velocity.x = move_toward(velocity.x, 0, current_speed)
 
-    # Animations.
-    if not is_rolling:
-        if is_on_floor():
-            if Input.is_action_just_pressed("roll"):
-                is_rolling = true
-                animated_sprite.play("roll")
-            elif direction:
-                animated_sprite.play("run")
-            else:
-                animated_sprite.play("idle")
-        else:
-            animated_sprite.play("air")
-
-    move_and_slide()
+func update_animation() -> void:
+    if player_state == STATE_ROLL:
+        return
+    
+    if not is_on_floor():
+        player_state = STATE_JUMP
+        animated_sprite.play(STATE_JUMP)
+    elif Input.is_action_just_pressed(STATE_ROLL):
+        player_state = STATE_ROLL
+        animated_sprite.play(STATE_ROLL)
+    elif abs(velocity.x) > 0:
+        player_state = STATE_SPRINT
+        animated_sprite.play(STATE_SPRINT)
+    else:
+        player_state = STATE_IDLE
+        animated_sprite.play(STATE_IDLE)
 
 func _on_animation_finished(animation_name: String) -> void:
-    if animation_name == "roll":
-        is_rolling = false
+    if animation_name == STATE_ROLL:
+        if is_on_floor():
+            if abs(velocity.x) > 0:
+                player_state = STATE_SPRINT
+                animated_sprite.play(STATE_SPRINT)
+            else:
+                player_state = STATE_IDLE
+                animated_sprite.play(STATE_IDLE)
+        else:
+            player_state = STATE_JUMP
+            animated_sprite.play(STATE_JUMP)
+    elif animation_name == STATE_DEATH:
+        queue_free()
+
+func die() -> void:
+    player_state = STATE_DEATH
+    animated_sprite.play(STATE_DEATH)
+    velocity = Vector2.ZERO
